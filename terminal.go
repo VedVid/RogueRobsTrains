9d5ad07bd66ccb14ef783b70e8816f0078838781
Vedor Vidurakis <toalak@gmail.com>
2019-03-03 22:10:07 +0100

    square font for map drawing

--- terminal.go
+++ terminal.go
@@ -67,6 +67,7 @@ func InitializeBLT() {
 	sizeFont := strconv.Itoa(FontSize)
 	window := "window: size=" + sizeX + "x" + sizeY
 	blt.Set(window + ", title=' " + GameTitle + "'; font: " + FontName + ", size=" + sizeFont)
+	blt.Set("draw font: " + FontName + ", size=" + sizeFont+"x"+sizeFont + ";")
 	blt.Clear()
 	blt.Refresh()
 }
