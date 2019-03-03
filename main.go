15faacdf5b41d911a5b1627c0164816aa39d6d5e
Vedor Vidurakis <toalak@gmail.com>
2019-03-03 15:50:22 +0100

    train3

--- main.go
+++ main.go
@@ -107,7 +107,7 @@ func NewGame(b *Board, c *Creatures, o *Objects) {
 	*c = Creatures{player, enemy}
 	*o = Objects{}
 	var c2 = Creatures{}
-	*b, c2, err = LoadJsonMap("train2.json")
+	*b, c2, err = LoadJsonMap("train3.json")
 	if err != nil {
 		fmt.Println(err)
 	}
