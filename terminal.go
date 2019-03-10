ae369bcdd644bacdd61487acdc8fb16b8b568ae1
Vedor Vidurakis <toalak@gmail.com>
2019-03-10 16:14:39 +0100

    updates

--- terminal.go
+++ terminal.go
@@ -47,7 +47,7 @@ const (
 	LogSizeY    = WindowSizeY - MapSizeY
 	LogPosX     = 0
 	LogPosY     = MapSizeY
-	GameTitle   = "Rogue Robs Trains v0.0.2b"
+	GameTitle   = "Rogue Robs Trains v0.0.2c"
 	FontName    = "Deferral-Square.ttf"
 	FontSize    = 14
 )
