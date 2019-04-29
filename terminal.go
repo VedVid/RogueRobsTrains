c49459bb22d52db09bcf8f94d8f5326a39370913
Vedor Vidurakis <toalak@gmail.com>
2019-04-29 19:45:14 +0200

    I_HATE_DEPLOYMENT

--- terminal.go
+++ terminal.go
@@ -47,7 +47,7 @@ const (
 	LogSizeY    = WindowSizeY - MapSizeY
 	LogPosX     = 0
 	LogPosY     = MapSizeY
-	GameTitle   = "Rogue Robs Trains v0.0.4b"
+	GameTitle   = "Rogue Robs Trains v0.0.4c"
 	FontName    = "Deferral-Square.ttf"
 	FontSize    = 14
 )
